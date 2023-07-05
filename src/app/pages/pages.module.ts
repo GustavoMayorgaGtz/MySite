dd8de074d8174f24b7d4f399b42c84279d5ac39e
GustavoMayorgaGtz <redvioletoficial@gmail.com>
2023-07-04 22:01:27 -0600

    first commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PagesRouting } from './pages.routing';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PagesRouting
+  ]
+})
+export class ModuleModule { }
