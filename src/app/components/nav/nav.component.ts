b8c0cc1831b3e5bd8015b6172e559257f35be605
GustavoMayorgaGtz <redvioletoficial@gmail.com>
2023-07-14 20:19:53 -0600

    Menu responsive animation create

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  constructor() {
 
+  }
+  ngOnInit(): void {
+
+  }
+
+  public classMenu = "hiddeMenu";
+  public isShowMenu = false;
+  responsiveMenuClick_Event(menuElement: HTMLDivElement) {
+    this.isShowMenu = !this.isShowMenu;
+    if (this.isShowMenu) {
+      this.classMenu = "showMenu";
+      menuElement.classList.remove("hiddeMenu");
+      menuElement.classList.toggle(this.classMenu)
+      // menuElement.setAttribute("class", "showMenu");
+    } else {
+      this.classMenu = "hiddeMenu";
+      menuElement.classList.remove("showMenu");
+      menuElement.classList.toggle(this.classMenu)
+      // menuElement.setAttribute("class", "hiddeMenu");
+    }
+    console.log(this.classMenu);  
+    // menuElement.classList.toggle(this.classMenu);
+    // menuElement.offsetHeight; // force DOM reflow
+    //  menuElement.style.animation = "2s "+ this.classMenu+";";
+    //  menuElement.style.animation = "2s "+ this.classMenu;
+  }
 }
