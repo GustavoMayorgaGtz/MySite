011c27980e0c6c8ea8fbf13bd821af2d0e418868
GustavoMayorgaGtz <redvioletoficial@gmail.com>
2023-07-22 12:15:49 -0600

    Guardando estilos

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.scss']
+  styleUrls: ['./inicio.component.scss', './inicio.component.mobile.scss']
 })
 export class InicioComponent {
 
